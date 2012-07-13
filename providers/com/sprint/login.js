9a44969929ebf7a587dea911570db4ef728e68ea
Eric J. Duran <eric.duran7@gmail.com>
2012-07-13 01:56:59 -0400

    intial commit

---
+++ providers/com/sprint/login.js
@@ -0,0 +1,43 @@
+// 1st take a scrapping bill data from sprint.com.
+
+var casper = require('casper').create(),
+    utils = require('utils'),
+    x = require('casper').selectXPath;
+
+// Login URL.
+casper.start('https://mysprint.sprint.com/mysprint/pages/sl/global/login.jsp');
+
+// Fill form out.
+casper.then(function() {
+  var user = casper.cli.get("user");
+  var pass = casper.cli.get("pass");
+  this.fill('form#frmUserLoginDL', {
+      'USER': user,
+      'PASSWORD':pass,
+  }, true);
+});
+
+// Go to bills
+// TODO: This assumes password is always correct.
+casper.then(function() {
+  this.clickLabel('See bill', 'a');
+});
+
+// Grab important data.
+casper.then(function() {
+  var data = {};
+  // Using xpatch because is super simple to grab with chrome.
+  // Also a bunch of tables with no class gets pretty bad, pretty quick.
+  data.prevTotal = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[7]/td/table/tbody/tr[1]/td[1]/table/tbody/tr/td[2]/b'));
+  data.total = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[7]/td/table/tbody/tr[7]/td[1]/table/tbody/tr/td[2]/font/b'));
+  data.account = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td/table/tbody/tr[3]/td[2]'));
+  data.billingCycle = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td/table/tbody/tr[3]/td[3]'));
+
+  // Dump it all to the screen.
+  utils.dump(data);
+});
+
+casper.run(function() {
+    this.exit();
+});
+
---
+++ readme.md
@@ -0,0 +1,36 @@
+
+NoNameYet
+---------
+
+An attempt at building an open source bill aggregator.
+
+The idea is to be able to easily collect bill information from all providers.
+
+Right now there is only one "supporter" provider (sprint). Only because I have and account with them.
+
+This assumes you have CasperJS.
+
+Example:
+```
+casperjs providers/com/sprint/login.js --user=YourUserName --pass=SuperSecretPassword
+```
+
+return:
+
+
+```
+{
+    "prevTotal": "$100.71",
+    "total": "$106.13",
+    "account": "123456789",
+    "billingCycle": "Jun 02-Jul 01"
+}
+````
+
+To add a provider you just need to create a new directory in a reverse dns folder structur under the providers directory (aka sprint.com => /providers/com/sprint/).
+
+In that directory you can have a login.js.
+
+I'm still not 100% sure on the format of these files, and I'm still thinking everything over.
+
+All ideas welcome.
