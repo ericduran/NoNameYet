968368c38ba25dd73b914c89667fd47e30b95ed4
Eric J. Duran <eric.duran7@gmail.com>
2012-07-14 22:10:30 -0400

    Add support for summary screen capture

--- providers/com/sprint/login.js
+++ providers/com/sprint/login.js
@@ -26,12 +26,24 @@ casper.then(function() {
 // Grab important data.
 casper.then(function() {
   var data = {};
+  var summary = x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[7]/td/table/tbody');
+
   // Using xpatch because is super simple to grab with chrome.
   // Also a bunch of tables with no class gets pretty bad, pretty quick.
   data.prevTotal = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[7]/td/table/tbody/tr[1]/td[1]/table/tbody/tr/td[2]/b'));
   data.total = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[7]/td/table/tbody/tr[7]/td[1]/table/tbody/tr/td[2]/font/b'));
   data.account = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td/table/tbody/tr[3]/td[2]'));
   data.billingCycle = this.fetchText(x('/html/body/table[1]/tbody/tr/td[2]/table/tbody/tr[5]/td/table/tbody/tr[1]/td/table/tbody/tr[3]/td[3]'));
+
+  // Clean up the screen selection first
+  // Remove the payment button, and links.
+  this.evaluate(function() {
+    jQuery('img[alt="Make Payment Now"]').hide();
+    jQuery('.generalLinks').hide().siblings().hide();
+  });
+
+  data.summaryUrl = data.account + '-sprint-summary.png';
+  this.captureSelector(data.summaryUrl, summary);
 
   // Dump it all to the screen.
   utils.dump(data);
@@ -40,4 +52,3 @@ casper.then(function() {
 casper.run(function() {
     this.exit();
 });
-
